13339985f980930f24c227a658519b17c343bb60
antonio12554 <antonio.almeida.biluca8@gmail.com>
2026-07-17 19:33:27 +0100

    add the Base

--- src/core/db.py
+++ src/core/db.py
@@ -1,10 +1,13 @@
 #database
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,DeclarativeBase
 #link of database
 from src.config import settings
 
 Engine = create_engine(settings.databse_url)
+
+class Base(DeclarativeBase):
+    pass
 
 SessionLocal = sessionmaker(bind=Engine,autoflush=False)
 
