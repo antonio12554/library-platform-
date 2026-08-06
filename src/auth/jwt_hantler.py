3db897e70658f746e4d3a743a10d0de5dd2066e4
antonio12554 <antonio.almeida.biluca8@gmail.com>
2026-08-06 19:03:03 +0100

    create the validate functions

--- src/auth/jwt_hantler.py
+++ src/auth/jwt_hantler.py
@@ -3,15 +3,21 @@ from datetime import datetime,timezone,timedelta
 #secret key
 from src.config import settings
 
-
-def generate_token(id_user: int):
+def generate_access_token(id_user: int):
     time = (datetime.now(timezone.utc) + timedelta(minutes=30)).time()
     payload: object = {"exp":time,"sub":id_user}
     access_token: str = jwt.encode(payload=payload, secret_key=settings.secret_key,algorithm=settings.algorithm)
     return access_token
 
-def decode_token(token: str):
-    payload = jwt.decode(token, settings.secret_key, settings.algorithm)
-    return payload
+def validate_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.secret_key,algorithms=[settings.algorithm])
+        return payload
+    except jwt.InvalidTokenError:
+        return None
+
+def get_user(payload: object):
+    user_id = payload.get("sub")
+    return user_id
 
     
